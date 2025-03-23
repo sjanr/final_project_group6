0e771d53b26462939fe198c765c94c26105f843c
aakash-tir <aakashtirathdas@gmail.com>
2025-03-22 18:16:28 -0700

    small change to weapon switch- successful

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -6,7 +6,8 @@ public class InputHandler : MonoBehaviour{
 
     public System.Action HammerBrickBreak;
     public System.Action GunBlockLauncher;
-    private char weapon = '1';
+    public enum WeaponType { Weapon1, Weapon2 }
+    private WeaponType currentWeapon = WeaponType.Weapon1;
 
     void Update(){
         
@@ -14,19 +15,19 @@ public class InputHandler : MonoBehaviour{
         if (Input.GetKeyDown(KeyCode.E))
         {
             //switch weapon
-            weapon = weapon == '1' ? '2' : '1';
-            Debug.Log("Switched to weapon " + weapon);
+             currentWeapon = currentWeapon == WeaponType.Weapon1 ? WeaponType.Weapon2 : WeaponType.Weapon1;
+            Debug.Log("switched to " + currentWeapon);
         }
 
         //attack with left click
         if (Input.GetMouseButtonDown(0))
         {
-            switch (weapon)
+            switch (currentWeapon)
             {
-                case '1':
+                case WeaponType.Weapon1:
                     HammerBrickBreak?.Invoke();
                     break;
-                case '2':
+                case WeaponType.Weapon2:
                     GunBlockLauncher?.Invoke();
                     break;
             }
