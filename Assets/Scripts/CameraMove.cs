74a185b296612e828695391c7eec4b7d5ad8a065
sjanr <97484144+sjanr@users.noreply.github.com>
2025-03-23 15:58:42 -0700

    Edited camera code to add a y offset

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private float yOffset = 2f; // Adjust this in the Inspector
 
     private void Update()
     {
-        this.transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(
+            player.position.x,
+            player.position.y + yOffset, // Add the offset here
+            this.transform.position.z
+        );
     }
-
 }
