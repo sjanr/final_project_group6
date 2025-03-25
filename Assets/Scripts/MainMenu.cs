d1758b0413a507e14cd552e3658e01d0b9c87a6c
Carson Bennett <carben2004@gmail.com>
2025-03-25 12:43:41 -0700

    |S| Added controls for pause menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,6 @@ public class MainMenu : MonoBehaviour
     }
     public void ExitGame()
     {
-
+        Application.Quit();
     }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public static bool isPaused = false;
+    [SerializeField] private GameObject PauseMenuUI;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
     public void RestartGame()
     {
-
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void ExitGame()
     {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
 
+    public void PauseGame()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     public void ResumeGame()
     {
-
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
