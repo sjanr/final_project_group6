e363907241063920c4aefe8643b70ac75a38b5b4
Carson Bennett <carben2004@gmail.com>
2025-03-25 12:12:20 -0700

    |U| Reconfigured Main Menu and Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void RestartGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+
+    }
+
+    public void ResumeGame()
+    {
+
+    }
+}
