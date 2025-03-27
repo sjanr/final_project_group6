c0a85b3eab3869f910dca77cf629325d442ba601
Carson Bennett <carben2004@gmail.com>
2025-03-27 09:09:13 -0700

    |S| Completed particle movement script

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem movementParticle;
+
+    [Range(0, 10)]
+    [SerializeField] int occurAfterVelocity;
+
+    [Range(0, 0.2f)]
+    [SerializeField] float dustFormationPeriod;
+
+    [SerializeField] Rigidbody2D playerRB;
+
+    float counter;
+
+    private void Update()
+    {
+        counter += Time.deltaTime;
+        if (Mathf.Abs(playerRB.linearVelocity.x) > occurAfterVelocity)
+        {
+            if (counter > dustFormationPeriod)
+            {
+                movementParticle.Play();
+                counter = 0;
+            }
+        }
+    }
+}
