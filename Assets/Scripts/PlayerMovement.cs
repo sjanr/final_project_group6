e212ee8d9c26b65c3378f09719dd53c7b5657baa
sjanr <97484144+sjanr@users.noreply.github.com>
2025-03-29 12:03:57 -0700

    Fixed and completed player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D playerBody;
+
     [Header("Player Speed")]
     [SerializeField] private float speed;
 
@@ -11,48 +12,54 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    [Header("Animation")]
     [SerializeField] private Animator animator;
+
     private bool grounded;
-    
+
     private void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
     }
 
-private void Update()
-{
-    grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-    float horizontalInput = Input.GetAxis("Horizontal");
-    playerBody.linearVelocity = new Vector2(horizontalInput * speed, playerBody.linearVelocity.y);
-
-    // Flip sprite
-    if (horizontalInput > 0.01f)
-        transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
-    else if (horizontalInput < -0.01f)
-        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-
-    // Animations
-    animator.SetBool("isRunning", Mathf.Abs(horizontalInput) > 0.01f);
-    animator.SetBool("isJumping", !grounded); // <- Set jump animation when NOT grounded
-
-    // Jump input
-    if (Input.GetKeyDown(KeyCode.Space) && grounded)
+    private void Update()
     {
-        Jump();
-        animator.SetBool("isJumping", true); // Force jump animation
+        // Check if grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Horizontal movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerBody.linearVelocity = new Vector2(horizontalInput * speed, playerBody.linearVelocity.y);
+
+        // Flip sprite based on movement direction
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+        // Animation parameters
+        animator.SetBool("isRunning", Mathf.Abs(horizontalInput) > 0.01f);
+        animator.SetBool("isInMidAir", !grounded);
+
+        // Jump input
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+            animator.Play("Jump", 0, 0f); // Layer 0, time 0
+            animator.SetBool("isJumping", true);
+        }
+
+
+        // Reset isJumping once airborne so it doesn't loop
+        if (!grounded && animator.GetBool("isJumping"))
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 
-    // Reset animation state on land
-    if (grounded)
-    {
-        animator.SetBool("isJumping", false);
-    }
-
-}
     private void Jump()
     {
         playerBody.linearVelocity = new Vector2(playerBody.linearVelocity.x, jumpHeight);
     }
-
 }
