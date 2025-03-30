14b8bae7f5d20c91857ed60dab7e116b31539018
sjanr <97484144+sjanr@users.noreply.github.com>
2025-03-30 15:37:37 -0700

    Finished animation code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D playerBody;
+
     [Header("Player Speed")]
     [SerializeField] private float speed;
 
@@ -11,8 +12,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+
     private bool grounded;
-    
+
     private void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
@@ -34,16 +39,24 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3((float)0.5, (float)0.5, (float)0.5);
         }
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        animator.SetBool("isRunning", Mathf.Abs(horizontalInput) > 0.01f);
+        animator.SetBool("isInMidAir", !grounded);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             Jump();
+            animator.Play("Jump", 0, 0f);
+            animator.SetBool("isJumping", true);
         }
 
+        if (!grounded && animator.GetBool("isJumping"))
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 
     private void Jump()
     {
         playerBody.linearVelocity = new Vector2(playerBody.linearVelocity.x, jumpHeight);
     }
-
 }
